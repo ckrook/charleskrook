6e955eff10f8c36440bdda3153ddf210b36e5d47
Charles Krook <charles.krook@gmail.com>
2025-05-20 15:59:39 +0200

    fix(page) layout styles in Home component; adjust padding and grid configuration for better responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default async function Home() {
   );
 
   return (
-    <main className=" mx-auto w-full max-w-[1400px]  flex mx-auto flex-col justify-between scroll-auto">
+    <main className="mx-auto w-full max-w-[1400px] flex flex-col justify-between scroll-auto px-4 md:px-0">
       <div className="flex flex-col items-start justify-start">
         <div className="py-24">
           <IntroSection />
@@ -82,7 +82,7 @@ export default async function Home() {
             <h2 className="text-xl md:text-2xl font-semibold mb-6 md:mb-8">
               Side Projects
             </h2>
-            <div className="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-3 gap-6 md:gap-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 md:gap-8">
               {sideProjects.map((project) => (
                 <div
                   className="bg-gray-50 h-128 rounded-xl p-4 border border-neutral-200 relative overflow-hidden"
