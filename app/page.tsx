f7613451ef23ce04369f35a2d9bee403044e9315
Charles Krook <charles.krook@gmail.com>
2025-09-29 21:07:26 +0200

    fix: design-adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ const Home = async () => {
   const selectedWork = projects.filter((project) => project.selectedWork);
 
   return (
-    <main className="col-start-1 col-end-13 grid grid-cols-4 sm:grid-cols-8 justify-between scroll-auto px-6 md:px-6">
+    <main className="col-start-1 col-end-13 grid grid-cols-4 sm:grid-cols-8 justify-between scroll-auto px-4 md:px-4">
       <HeroSection />
       <WorkSection selectedWork={selectedWork} />
       <ClientSection clients={clients} />
