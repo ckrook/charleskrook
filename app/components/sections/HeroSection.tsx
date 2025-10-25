02b8f0b806eefcc0b54649be877ed0defa91efde
Charles Krook <charles.krook@gmail.com>
2025-10-25 08:44:56 +0200

    fix: new-google-image

--- app/components/sections/HeroSection.tsx
+++ app/components/sections/HeroSection.tsx
@@ -1,11 +1,24 @@
 import { FadeInOnScroll } from "../FadeInOnScroll";
+import Image from "next/image";
 
 const HeroSection = () => {
   return (
     <FadeInOnScroll duration={1.2} y={80}>
-      <div className="col-span-4 sm:col-start-1 sm:col-end-9 h-[483px] bg-figma-background-primary">
+      <div className="col-span-4 sm:col-start-1 sm:col-end-9 h-[483px] bg-figma-background-primary relative">
+        {/* Hero Image for Google Search Results */}
+
         {/* SEO content - hidden visually but accessible to search engines */}
         <div className="sr-only">
+          <div className="absolute inset-0 flex items-center justify-center">
+            <Image
+              src="/thumb-img.png"
+              alt="Charles Krook - Fullstack Engineer"
+              width={200}
+              height={200}
+              className="rounded-lg"
+              priority
+            />
+          </div>
           <h1>Charles Krook</h1>
           <p>Fullstack Engineer</p>
           <p>
